bd2a3a06659fb6998124d715ae1351b6bc676369
alithampan <alinh1014@gmail.com>
2025-06-01 11:56:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function toggleLanguage() {
+  alert('Fitur ganti bahasa masih dalam pengembangan!');
+}
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Form dikirim! (Integrasi email perlu EmailJS atau server)');
+});
